464cc321cbf9d77de72f690ce458e6081b545435
Matthias Jost <matthias.jost@gmail.com>
2021-02-11 13:07:16 +0100

    counters

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -10,6 +10,22 @@ class Counters extends Component {
     ],
   };
 
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
   handleDelete = (counterId) => {
     console.log(counterId);
 
@@ -22,10 +38,14 @@ class Counters extends Component {
   render() {
     return (
       <div>
+        <button onClick={this.handleReset} className="btn-primary btn-sm m-2">
+          Reset
+        </button>
         {this.state.counters.map((counter) => (
           <Counter
             key={counter.id}
             onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
             counter={counter}
           ></Counter>
         ))}
